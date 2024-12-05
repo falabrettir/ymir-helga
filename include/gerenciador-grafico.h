572267d9ad533663eb6a1d96f5666fa14a150200
Eduardo Dias <eduvidias@gmail.com>
2024-12-05 19:23:08 -0300

    Criação da classe gerenciador gráfico

--- include/gerenciador-grafico.h
+++ include/gerenciador-grafico.h
@@ -1,7 +1,18 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+namespace Gerenciadores {
+
 class Gerenciador_Grafico {
+private:
+  sf::RenderWindow janela;
+
 public:
   Gerenciador_Grafico();
+
+  ~Gerenciador_Grafico();
+
+  void desenharEnte(/*TODO*/);
 };
+
+} // namespace Gerenciadores
