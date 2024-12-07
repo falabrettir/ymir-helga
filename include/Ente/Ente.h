d72a5263a39ea01d8d1ea8fb4a34d790a6531b9c
falabrettir <falabretti@alunos.utfpr.edu.br>
2024-12-07 16:53:14 -0300

    Sprites and Textures

--- include/Ente/Ente.h
+++ include/Ente/Ente.h
@@ -14,6 +14,8 @@ public:
   Ente();
   ~Ente();
   void setGerenciadorGrafico(Gerenciadores::Gerenciador_Grafico *pGG);
+  bool setTexture(const std::string &path);
+  bool setSprite(const std::string &path);
   virtual void desenhar();
   virtual void executar() = 0;
 };
