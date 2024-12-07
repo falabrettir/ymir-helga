9c9b0507ebef65bb363ec8484240506a94a4bdb5
falabrettir <falabretti@alunos.utfpr.edu.br>
2024-12-07 11:34:37 -0300

    Add: Assets

--- include/Ente/Ente.h
+++ include/Ente/Ente.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "../gerenciadores/gerenciador-grafico.h"
-#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
 
 class Ente {
 protected:
@@ -8,11 +8,12 @@ protected:
   static Gerenciadores::Gerenciador_Grafico *pGG;
   static int cont;
   sf::Texture *texture;
+  sf::Sprite *sprite;
 
 public:
   Ente();
   ~Ente();
   void setGerenciadorGrafico(Gerenciadores::Gerenciador_Grafico *pGG);
-  virtual void desenhar() = 0;
+  virtual void desenhar();
   virtual void executar() = 0;
 };
--- src/Ente/Ente.cpp
+++ src/Ente/Ente.cpp
@@ -3,8 +3,9 @@
 int Ente::cont(0);
 Gerenciadores::Gerenciador_Grafico *pGG(NULL);
 
-Ente::Ente() : id(cont++), texture(NULL) {}
+Ente::Ente() : id(cont++), texture(nullptr), sprite(nullptr) {}
 Ente::~Ente() {}
 
 void Ente::desenhar() {}
+
 void Ente::executar() {}
