ce1a85eb8712e3190971ae594043f2da6cf88bb0
Eduardo Dias <eduvidias@gmail.com>
2025-01-04 20:44:08 -0300

    Criando o map de teclas

--- src/Gerenciadores/GerenciadorInput.cpp
+++ src/Gerenciadores/GerenciadorInput.cpp
@@ -1,13 +1,24 @@
 #include "Gerenciadores/GerenciadorInput.h"
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/Window.hpp>
 
 using namespace Gerenciadores;
 
 Gerenciador_Input *Gerenciador_Input::instancia = nullptr;
 
-Gerenciador_Input::Gerenciador_Input() {}
+Gerenciador_Input::Gerenciador_Input() {
+  keyMap.clear();
+  keyMap[sf::Keyboard::A] = "A";
+  keyMap[sf::Keyboard::D] = "D";
+  keyMap[sf::Keyboard::W] = "W";
+  keyMap[sf::Keyboard::Space] = "Space";
+  keyMap[sf::Keyboard::Left] = "Left";
+  keyMap[sf::Keyboard::Right] = "Right";
+  keyMap[sf::Keyboard::Up] = "Up";
+  keyMap[sf::Keyboard::RShift] = "RShift";
+}
 
-Gerenciador_Input::~Gerenciador_Input() {}
+Gerenciador_Input::~Gerenciador_Input() { keyMap.clear(); }
 
 Gerenciador_Input *Gerenciador_Input::getInstancia() {
   if (instancia == nullptr) {
@@ -17,3 +28,5 @@ Gerenciador_Input *Gerenciador_Input::getInstancia() {
 }
 
 void Gerenciador_Input::setGG(Gerenciador_Grafico *ppGG) { pGG = ppGG; }
+
+void Gerenciador_Input::processaInput() {}
