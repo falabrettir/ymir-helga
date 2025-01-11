5dfd8a80fc515dde721da56dc6a67b5d2c57f1d8
Eduardo Dias <eduvidias@gmail.com>
2025-01-11 16:48:44 -0300

    Mudando o nome da variavel de Cont pra Contr, pra nao confundir com um contador

--- include/Entidades/Personagens/Skjolder.h
+++ include/Entidades/Personagens/Skjolder.h
@@ -1,10 +1,13 @@
 #include "Controladores/ControladorJogador.h"
 #include "Jogador.h"
+
+namespace Entidades::Personagens {
+
 class Skjolder : public Entidades::Personagens::Jogador {
 private:
   float stamina;
   short int furia;
-  Controladores::Controlador_Jogador *pCont;
+  Controladores::Controlador_Jogador *pContr;
 
 public:
   Skjolder();
@@ -13,3 +16,5 @@ public:
   void atacar();
   void executar();
 };
+
+} // namespace Entidades::Personagens
--- src/Entidades/Personagens/Skjolder.cpp
+++ src/Entidades/Personagens/Skjolder.cpp
@@ -1,16 +1,20 @@
 #include "Entidades/Personagens/Skjolder.h"
 #include "Controladores/ControladorJogador.h"
 
-Skjolder::Skjolder() : Jogador(), stamina(100.0f), furia(0), pCont(nullptr) {
+namespace Entidades::Personagens {
+
+Skjolder::Skjolder() : Jogador(), stamina(100.0f), furia(0), pContr(nullptr) {
   setTexture("/assets/Characters(100x100)/Knight/Knight/Knight-Idle.png");
-  pCont = new Controladores::Controlador_Jogador();
-  pCont->setJog(this);
+  pContr = new Controladores::Controlador_Jogador();
+  pContr->setJog(this);
 }
 Controladores::Controlador_Jogador *Skjolder::getControlador() const {
-  return pCont;
+  return pContr;
 }
 // TODO:
 // Destrutora e metodos atacar e executar
 Skjolder::~Skjolder() {}
 void Skjolder::atacar() {}
 void Skjolder::executar() {}
+
+} // namespace Entidades::Personagens
