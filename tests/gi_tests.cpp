85006c086d294e66a00bedec38fd13573e0f9a28
Eduardo Dias <eduvidias@gmail.com>
2025-01-15 22:36:32 -0300

    Testes feitos com o copilot

--- tests/gi_tests.cpp
+++ tests/gi_tests.cpp
@@ -1,3 +1,4 @@
+#include "Entidades/Personagens/Helga.h"
 #include "Entidades/Personagens/Skjolder.h"
 #include "Gerenciadores/GerenciadorInput.h"
 #include <catch2/catch.hpp>
@@ -15,6 +16,8 @@ TEST_CASE("Gerenciador input") {
     Entidades::Personagens::Skjolder *pJog =
         new Entidades::Personagens::Skjolder();
 
+    Entidades::Personagens::Helga *pHelga = new Entidades::Personagens::Helga();
+
     SECTION("Lista de observadores deve ser inicializada como nullptr") {
       REQUIRE(pGI->observadores == nullptr);
     }
@@ -25,8 +28,15 @@ TEST_CASE("Gerenciador input") {
       REQUIRE(pGI->observadores != nullptr);
       REQUIRE(pGI->observadores->size() == 1);
 
+      pGI->inscrever(static_cast<Observer *>(pHelga->getControlador()));
+
+      REQUIRE(pGI->observadores->size() == 2);
+
       pGI->desinscrever(static_cast<Observer *>(pJog->getControlador()));
-      REQUIRE(pGI->observadores->empty() == true);
+      REQUIRE(pGI->observadores->size() == 1);
+
+      pGI->desinscrever(static_cast<Observer *>(pHelga->getControlador()));
+      REQUIRE(pGI->observadores->size() == 0);
     }
   }
 }
