cb6fcfb713579284772e5b1f28e8ed49939207ac
falabrettir <falabretti@alunos.utfpr.edu.br>
2025-01-18 15:50:13 -0300

    Animacao concluida, Fix em ListaEntidade

--- src/Listas/ListaEntidade.cpp
+++ src/Listas/ListaEntidade.cpp
@@ -1,17 +1,25 @@
+#include "Listas/Lista.h"
 #include "Listas/ListaEntidades.h"
-
 namespace Listas {
 ListaEntidades::ListaEntidades() : LEs() {}
-ListaEntidades::~ListaEntidades() { LEs.limpar(); }
-void ListaEntidades::incluir(Entidades::Entidade *pE) { LEs.incluir(pE); }
-void ListaEntidades::deletar(Entidades::Entidade *pE) { LEs.remover(pE); }
+ListaEntidades::~ListaEntidades() { limpar(); }
+void ListaEntidades::incluir(Entidades::Entidade *e) { LEs.incluir(e); }
+void ListaEntidades::deletar(Entidades::Entidade *e) { LEs.remover(e); }
 const int ListaEntidades::getSize() const { return LEs.getSize(); }
 void ListaEntidades::limpar() { LEs.limpar(); }
-Lista<Entidades::Entidade>::Iterator ListaEntidades::begin() const {
-  return LEs.begin();
+const Listas::Lista<Entidades::Entidade *>::Iterator
+ListaEntidades::begin() const {
+  return this->begin();
 }
-Lista<Entidades::Entidade>::Iterator ListaEntidades::end() const {
-  return LEs.end();
+const Listas::Lista<Entidades::Entidade *>::Iterator
+ListaEntidades::end() const {
+  return this->end();
 }
-
+void ListaEntidades::percorrer() {
+  Entidades::Entidade *aux = nullptr;
+  for (int i = 0; i < LEs.getSize(); i++) {
+    aux = LEs.operator[](i);
+    aux->executar();
+  }
+}
 } // namespace Listas
