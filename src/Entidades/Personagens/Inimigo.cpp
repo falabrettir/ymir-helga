058ac2fb7bff3a4d6b0ad3102e2048474fc0e9b0
falabrettir <falabretti@alunos.utfpr.edu.br>
2025-02-04 16:56:09 -0300

    Feat: Movimento aleatório em Inimigo

--- src/Entidades/Personagens/Inimigo.cpp
+++ src/Entidades/Personagens/Inimigo.cpp
@@ -11,7 +11,7 @@ namespace Entidades::Personagens::Inimigos {
 
 std::set<Jogador *> Inimigo::setJogadores{};
 
-Inimigo::Inimigo(ID id) : Personagem(id), visada(500.f), visando(false) {}
+Inimigo::Inimigo(ID id) : Personagem(id), visada(50.f), visando(false) {}
 
 Inimigo::~Inimigo() {}
 
@@ -23,7 +23,9 @@ void Inimigo::perseguir() {
   std::set<Jogador *>::iterator it = setJogadores.begin();
   Jogador *aux = nullptr;
   float atual, min = visada;
+  visando = false;  // Reset targeting flag
 
+  // Check for players in sight
   while (it != setJogadores.end()) {
     atual = calculaDistancia(*it);
     if (atual <= min) {
@@ -33,19 +35,33 @@ void Inimigo::perseguir() {
     }
     ++it;
   }
-  setVelX(MAXVEL * 0.5);
+
+  // If targeting player, chase them
   if (aux != nullptr) {
+    setVelX(MAXVEL * 0.5);
     sf::Vector2f pos = this->getPos();
     sf::Vector2f jogpos = aux->getPos();
-
-    sf::Vector2f direcao = jogpos - pos;  // Fix direction
+    sf::Vector2f direcao = jogpos - pos;
     float magnitude = std::sqrt(direcao.x * direcao.x + direcao.y * direcao.y);
-
-    if (magnitude != 0) direcao /= magnitude;  // Normalize correctly
-
+    if (magnitude != 0) direcao /= magnitude;
     sf::Vector2f novaVel;
     novaVel.x = direcao.x * getVel().x;
     setVelX(novaVel.x);
+  } else {  // movimento aleatório
+    static float moveTimer = 0.0f;
+    static int direcao = 1;
+    static float mudarIntervalo = (rand() % 20 + 10) / 10.0f;  // 1-3 seconds
+
+    moveTimer += 0.016f;
+
+    // Random chance to change direction each frame (10%)
+    if ((rand() % 100) < 10 || moveTimer >= mudarIntervalo) {
+      direcao = (rand() % 2) * 2 - 1;  // Generates either -1 or 1
+      moveTimer = 0.0f;
+      mudarIntervalo = (rand() % 20 + 10) / 10.0f;  // New random interval
+    }
+
+    setVelX(MAXVEL * 0.3 * direcao);
   }
 }
 const bool Inimigo::getVisando() const { return visando; }
